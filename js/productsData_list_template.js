388a4c2138f96d30810a72683d502b62a3cc4685
carla <second9040@gmail.com>
2022-09-07 23:28:21 +0800

    切版 - 波浪頁/section2/圖片

--- js/productsData_list_template.js
+++ js/productsData_list_template.js
@@ -10,7 +10,7 @@ export function getProductData(){
 function productList_temp(getData){
     let list_template = ``;
     getData.forEach( item => {
-        list_template += `<div class="products_box_item grid-col-mb-12 grid-col-m-4"><a href="products_content.html?${item.product_link}">${item.product_name}</a></div>`
+        list_template += `<div class="products_box_item grid-col-mb-12 grid-col-m-4"><a href="products_${item.product_link}">${item.product_name}</a></div>`
     });
     $('#products_box').html(list_template)
 }
