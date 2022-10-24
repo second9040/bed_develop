7d7c146fd20c2d58eda12fc37585ecef0ae327bb
carla <second9040@gmail.com>
2022-10-24 23:10:52 +0800

    商品頁陳列方式調整 fetch印出div模板調整

--- js/productsData_list_template.js
+++ js/productsData_list_template.js
@@ -1,6 +1,6 @@
 export function getProductData(){
     // 好眠商品資料獲取
-    fetch('json/products_items.json')
+    fetch('json/products_items.json?2')
     .then(res => {
         return res.json();
     })
@@ -19,7 +19,7 @@ export function getProductData(){
     
 };
 const main_product_url = location.href;
-let main_product_id;
+let main_product_id = 0;
 if(main_product_url.indexOf('?') != -1){
     main_product_id = main_product_url.split('id=')[1]
 }
@@ -29,13 +29,19 @@ function productList_temp(getData){
     let count = 1;
     getData.forEach( (item, index) => {
         if (item.main_product_id === main_product_id) {
+            console.log(item)
+            if (count % 3 === 1) {
+                list_template += `<div class="products_box_line line${(count/3 + 1).toFixed(0)}">`
+            }
             list_template += `
                 <div class="products_box_item">
                     <a href="${item.product_link}">
-                        <span class="product_img"><img src="images/limited/limited_item_0${count++}.jpg"></span>
-                        <span class="product_name">${item.product_name}</span>
+                        <span class="product_img"><img src="images/products/${item.category}/${item.category}_${count++}.png"></span>
                     </a>
                 </div>`;
+            if (count % 3 === 1) {
+                list_template += `</div>`;
+            }
         }
     })
     $('#products_box').html(list_template)
