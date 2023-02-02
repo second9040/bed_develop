8a2b9287068c9f16bafb9e96ee03fecb993da980
carla <second9040@gmail.com>
2023-02-02 22:54:04 +0800

    床架商品頁加上尺寸與輪播展示區塊

--- js/js_template.js
+++ js/js_template.js
@@ -1,3 +1,7 @@
 import {headerTemp, footerTemp} from "./template/common_module.js?0202-02";
 headerTemp();
 footerTemp();
+
+$('.inquire').click(() => {
+  window.open('https://lin.ee/KwKuDdB');
+})
