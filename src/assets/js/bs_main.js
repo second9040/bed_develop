45d77843be98290621df26eb5bd76137c978d0e3
second9040 <second9040@gmail.com>
2024-06-14 23:00:15 +0800

    把 bootstrap 版型改成 vite + vue3

--- src/assets/js/bs_main.js
+++ src/assets/js/bs_main.js
@@ -159,17 +159,6 @@
   });
 
   /**
-   * Init swiper sliders
-   */
-  function initSwiper() {
-    document.querySelectorAll('.swiper').forEach(function(swiper) {
-      let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
-      new Swiper(swiper, config);
-    });
-  }
-  window.addEventListener('load', initSwiper);
-
-  /**
    * Correct scrolling position upon page load for URLs containing hash links.
    */
   window.addEventListener('load', function(e) {
